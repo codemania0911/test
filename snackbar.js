56642427c1c484074e7cd524125331519cf1fa9a
slarkjm0803 <slarkjm@outlook.com>
2020-03-27 22:39:20 +0800

    dev commit

--- snackbar.js
+++ snackbar.js
@@ -5,21 +5,9 @@ export const snackbar = {
     show: false,
   },
   mutations: {
-    SET_SHOW (state, data) {
+    DEV (state, data) {
       state.show = data
     },
-    SET_TEXT (state, data) {
-      state.text = data
-    },
-    SET_COLOR (state, data) {
-      state.color = data
-    },
-  },
-  actions: {
-    showSnackBar ({ commit }, data) {
-      if (data.text) commit('SET_TEXT', data.text)
-      if (data.color) commit('SET_COLOR', data.color)
-      commit('SET_SHOW', true)
-    },
-  },
+    
+  }
 }
